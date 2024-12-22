066f8f29fd150eaf93ec69478be9426c4bcce884
catalincovali <catalincovali@icloud.com>
2024-12-22 01:33:36 +0100

    update still no implementation

--- include/DeviceManager.h
+++ include/DeviceManager.h
@@ -1,28 +1,31 @@
+//nome: CATALIN COVALI
 #ifndef DEVICEMANAGER_H
 #define DEVICEMANAGER_H
 #include <vector>
-#include <map>
 
 class DeviseManager {
 private:
   std::vector<Device*> devices;
-  double powerLimit;        // Maximum power that can be absorbed from the grid (3.5 kW)
-  double currentPower;      // Current power usage
+  double powerLimit;                // Maximum power that can be absorbed from the grid (3.5 kW)
+  double currentPower;              // Current power usage
+  std::vector<Device*> devicesON;   // needed for powerLimitPolicy
 
   Time currentTime(00,00);  //?
 
 public:
   DeviseManager(double maxPower=3.5) : powerLimit{maxPower}, currentPower{0.0} }
+  Time getCurrentTime() { return currentTime; }
 
   void addDevice(Device* device);
 
   void turnOnDevice(Device* device);
   void turnOffDevice(Device* device);
-  void setTimer(Device* device, /* ? */ );
+  void setTimer(Device* device, Time time );
   void removeTimer(Device* device);
 
   double getPowerConsumption() const;
 
+  // Shuts down devices in reverse order until power consumption is less than production
   void powerLimitPolicy();
 
   //For debug purposes
