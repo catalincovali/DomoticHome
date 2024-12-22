17e00a38f16f58dc6ea2b607cb592f90a6160941
catalincovali <catalincovali@icloud.com>
2024-12-22 23:05:15 +0100

    some functions are implemented

--- include/DeviceManager.h
+++ include/DeviceManager.h
@@ -5,34 +5,37 @@
 #include <vector>
 #include <stdexcept>
 #include "Device.h"
+#include "Time.
 
-class DeviseManager {
+class DeviceManager {
 private:
-  std::vector<Device*> devices;
-  double powerLimit;                // Maximum power that can be absorbed from the grid (3.5 kW)
-  double powerUsage;                // Current power usage
-  std::vector<Device*> devicesON;   // needed for powerLimitPolicy
+  std::vector< std::shared_ptr<Device> > devices;
+  double powerLimit;    // Maximum power that can be absorbed from the grid (3.5 kW)
+  double powerUsage;    // Current power usage
+  std::vector< std::shared_ptr<Device> > activeDevices;   // needed for powerLimitPolicy
 
-  Time currentTime(00,00);  //?
+  Time currentTime(0,0);
 
 public:
-  DeviseManager(double maxPower=3.5);
+  DeviceManager(double maxPower=3.5);
   Time getCurrentTime() { return currentTime; }
 
-  void addDevice(Device* device);
+  void addDevice( std::shared_ptr<Device> d );
+  void turnOnDevice( std::shared_ptr<Device> d );
+  void turnOffDevice( std::shared_ptr<Device> d );
+  void setTimer( std::shared_ptr<Device> d, Time time );
+  void removeTimer( std::shared_ptr<Device> d );
 
-  void turnOnDevice(Device* device);
-  void turnOffDevice(Device* device);
-  void setTimer(Device* device, Time time );
-  void removeTimer(Device* device);
-
-  double getPowerConsumption() const;
+  double getPowerUsage() const;
 
   // Shuts down devices in reverse order until power consumption is less than production
   void powerLimitPolicy();
 
+  // Simulates the passage of time by incrementing the current time by one minute.
+  // It updates device states and handles time-based events.
+  void setTime(Time time);
+
   //For debug purposes
-  void timeSimulations(Time time);
   void resetTime();
   void resetTimers();
   void resetAll();
--- src/DeviceManager.cpp
+++ src/DeviceManager.cpp
@@ -4,10 +4,90 @@
 #include <stdexcept>
 #include "DeviceManager.h"
 #include "Device.h"
+#include "Time.h"
 
 DeviceManger::DeviceManager(double maxPower) : powerLimit{maxPower}, powerUsage{0.0} {
   if (maxPower < 0)
     throw std::invlaid_argument("Errore: La potenza massima non puo essere negativa.");
 }
 
+void DeviceManager::addDevice(std::shared_ptr<Device> d) {
+  devices.push_back(d);
+}
+
+
+void DeviceManager::turnOnDevice(std::shared_ptr<Device> d){
+  if (d->isOn())
+    std::cout << currentTime << " Il dispositivo '" << d->getName() << "è già acceso\n";
+
+  while (d->getKW() + DeviceManager::getPowerUsage > powerLimit) { //[?]
+    DeviceManger::powerLimitPolicy();
+  }
+
+  d->turnOn();
+  activeDevices.push_back();
+}
+
+
+void DeviceManager::turnOffDevice(std::shared_ptr<Device> d){
+  if (!d->isOn())
+    std::cout << currentTime << " Il dispositivo '" << d->getName() << "è già spento\n";
+
+  auto it = std::find(activeDevices.begin(), activeDevices.end(), d);
+  if (it != activeDevices.end())
+    activeDevices.erese(it);
+
+  d->turnOff();
+
+
+}
+
+
+double DeviceManager::getPowerUsage() const {
+  powerUsage = 0.0;
+  for (const auto& deivce : devices)
+    if (device -> isOn())
+      powerUsage += device->getKW(); //[?]
+
+  return powerUsage;
+}
+
+
+void DeviceManager::powerLimitPolicy() {
+  std::shared_ptr<Device> lastDeviceTurnedOn;
+  lastDeviceTurnOn = activeDevices.back();
+  activeDevices.pop_back();
+  DeviceManager::turnOffDevice(lastDeviceTurnedOn);
+}
+
+void setTime(Time time) {
+  if(time < currentTime)
+    throw std::invalid_argument("Errore: Non si puo tornare indietro nel tempo");
+
+  while (currentTime.increment() != time) {
+    for (const auto& device : devices){
+      if (device->getStartTime() == currentTime)
+        DeviceManger::turnOnDevice(device);
+      if (device->getFinishTime() == currentTime)
+        DeviceManger::turnOffDevice(device);
+    }
+  }
+}
+
+// For debug purposes
+void DeviceManager::resetTime(){
+  currentTime.reset();//[?]
+}
+
+void DeviceManager::resetTimers(){
+  for (const auto& device : devices){
+    device -> setStartTime(0,-1);//[?]
+    device -> setFinishTime(0,-1);//[?]
+  }
+}
+
+void DeviceManager::resetAll(){
+  DeviceManger::resetTime()
+  DeviceManger::resetTimers()
+}
 
