3e2b135933af1e3cc329105ced815dc905a38a5e
kiki30072004 <gavrilovskakristina2004@gmail.com>
2025-01-07 22:05:37 +0100

    Create Time.h

---
+++ include/Time.h
@@ -0,0 +1,35 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <ostream>
+
+class Time{
+	
+	private:
+		int hours;
+		int minutes;
+		
+			
+	public:
+	
+		Time(int h=0, int m=0);
+		
+		void increment(void);
+		std::string toString(bool withBrackets = true) const;			//stampa formato [hh:mm] se withBrackets è true, e hh:mm se false
+		int toMinutes(void) const;
+		
+		bool operator <(const Time& t) const;
+		bool operator >(const Time& t) const;
+		bool operator ==(const Time& t) const;
+		bool operator !=(const Time& t) const;
+		Time operator +(int t);						//tempo formito in minuti
+		Time operator -(const Time& t) const;		//sottrazione tra oggetti tempo
+		
+		//ELIMINARE SE NON SERVE A NESSUNO
+		friend std::ostream& operator <<(std::ostream& out, const Time& t);		//stampa formato hh:mm
+};
+
+		Time stringToTime(std::string t);
+
+
+#endif
