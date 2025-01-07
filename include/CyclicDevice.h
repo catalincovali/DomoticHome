19f345eff2a8a472977fc105fc3d4654edfc2be6
kiki30072004 <gavrilovskakristina2004@gmail.com>
2025-01-07 20:08:54 +0100

    Create CyclicDevice.h

---
+++ include/CyclicDevice.h
@@ -0,0 +1,17 @@
+#ifndef CYCLICDEVICE_H
+#define CYCLICDEVICE_H
+#include "Device.h"
+#include "Time.h"
+
+class CyclicDevice : public Device {
+private:
+    Time cycleDuration;  //cycle duration of cyclic devices
+public:
+
+    CyclicDevice(std::string n, double power, Time cycleDuration);
+    Time getCycleDuration() const {return cycleDuration;}             // getter function of cycleDuration
+    void setProgrammedStart(const Time& time)override;
+    void setProgrammedStop(const Time& t)override;
+    void setStart(const Time& st) override;
+};
+#endif // CYCLICDEVICE_H
