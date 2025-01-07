58d778db674c1c543b730f0fcf9001ee62d5fe03
kiki30072004 <gavrilovskakristina2004@gmail.com>
2025-01-07 22:04:08 +0100

    Update ManualDevice.cpp

--- src/ManualDevice.cpp
+++ src/ManualDevice.cpp
@@ -2,18 +2,3 @@
 
 ManualDevice::ManualDevice(std::string n, double power)
     : Device(n, power) {}
-
-void ManualDevice::setProgrammedStart(const Time& time){
-    programmedStart = time;
-}
-
-void ManualDevice::setProgrammedStop(const Time& t){
-    if(programmedStart<t)  {
-             programmedStop = t;
-             isProgramValid=true;
-     }
-    else {
-        throw std::invalid_argument("programmed stop time must be after programmed start time"); 
-        isProgramValid=false;
-    }
-}
