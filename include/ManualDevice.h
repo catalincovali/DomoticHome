cac44d0ae126e6a5765520030d90459accb474ef
kiki30072004 <gavrilovskakristina2004@gmail.com>
2025-01-07 22:04:32 +0100

    Update ManualDevice.h

--- include/ManualDevice.h
+++ include/ManualDevice.h
@@ -6,8 +6,5 @@
 class ManualDevice : public Device {
 public:
     ManualDevice(std::string n, double power);
-
-    void setProgrammedStart(const Time& time) override;
-    void setProgrammedStop(const Time& t) override;
 };
 #endif // MANUALDEVICE_H
