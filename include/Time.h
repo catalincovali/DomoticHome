320321c8eee7e0822765768c9fc6095b8505eb84
LucaBortolazzi <luca.bortolazzi@studenti.unipd.it>
2025-01-09 14:46:00 +0100

    Update Time.h
    aggiunto commenti

--- include/Time.h
+++ include/Time.h
@@ -12,17 +12,17 @@ class Time{
 			
 	public:
 	
-		Time(int h=0, int m=0);
+		Time(int h=0, int m=0);		//se non forniti imposta 00:00 di default
 		
 		void increment(void);
 		std::string toString(bool withBrackets = true) const;		//stampa [hh:mm] se withBrackets è true, e hh:mm se false
-		int toMinutes(void) const;
+		int toMinutes(void) const;		//oggetto time in minuti
 		
 		bool operator <(const Time& t) const;
 		bool operator >(const Time& t) const;
 		bool operator ==(const Time& t) const;
 		bool operator !=(const Time& t) const;
-		Time operator +(int t);						//tempo formito in minuti
+		Time operator +(int t);				//tempo "t" formito in minuti
 		Time operator -(const Time& t) const;		//sottrazione tra oggetti tempo
 		
 		//ELIMINARE SE NON SERVE A NESSUNO
